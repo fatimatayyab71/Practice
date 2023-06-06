d9a07bc9109ed37e0abf6804a703d70b451a1668
Fatima Tayyab <fatimatayyab71@gmail.com>
2023-06-07 00:54:28 +0500

    first commit - committing all files to the repository

---
+++ main.c
@@ -0,0 +1,32 @@
+/*
+ * my slave.c
+ *
+ * Created: 1/5/2021 10:36:23 AM
+ * Author : Lenovo
+ */ 
+
+#define F_CPU 16000000
+#include <avr/io.h>
+#include "LCD.h"
+#define MISO 4
+
+
+int main(void)
+{
+	
+	
+	LCD_Init();
+	
+	DDRB = (1<<MISO);
+	SPCR = (1<<SPE);
+	
+	while (1)
+	{
+		//SPDR = 'G';
+		while(!(SPSR &(1<<SPIF)));
+		LCD_Data(SPDR);
+	}
+	
+}
+
+
